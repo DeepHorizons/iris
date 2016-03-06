ae143a9a3dfc084692f2a6e903691e01a631c8e6
Shawn Chowdhury <shawn.rc0@gmail.com>
2016-03-06 01:08:58 -0500

    reintroduced grammar

--- iris/plugins/iris_weather/__init__.py
+++ iris/plugins/iris_weather/__init__.py
@@ -1,8 +1,9 @@
 """
-
 """
 from iris.plugins import Plugin
 
 
 class IrisWeather(Plugin):
-    pass
+    grammar = {
+        "Whats the weather": str,  # Static methods store the function under __func__
+    }
