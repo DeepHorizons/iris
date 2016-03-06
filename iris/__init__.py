505bea049e6a54ceaf6f2993079318d9cec1caa2
Shawn Chowdhury <shawn.rc0@gmail.com>
2016-03-05 23:50:26 -0500

    dummy endpoint

--- iris/__init__.py
+++ iris/__init__.py
@@ -1,5 +1,38 @@
+#!/usr/bin/env python3
 """
 Core application logic for the central Iris Server
 
-:
+Shawn Chowdhury
+since 2016-03-05
+
+Built at BrickHack II
 """
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+
+# initialize
+app = Flask(__name__)
+api = Api(app)
+
+
+# === API Layer
+parser = reqparse.RequestParser()
+parser.add_argument('string', type=str, help="Natural language string to parse")
+
+
+class NLPEvaluate(Resource):
+
+    def post(self):
+
+        args = parser.parse_args()
+
+
+
+
+        return {'data': 'fooBar'}, 200
+
+
+# === build API endpoints
+api.add_resource(NLPEvaluate, '/api/evaluate')
